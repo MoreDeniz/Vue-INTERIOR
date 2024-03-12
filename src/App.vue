236f747f41a21d954276d34da5aa7d64992e59ce
maya <you@example.com>
2024-03-12 23:58:21 +0700

    commit 1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComp @changeContent="changeContent" />
+    <ContentComp :componentType="componentType" />
+    <FooterComp @changeContent="changeContent" />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContentComp from './components/ContentComp.vue'
+import HeaderComp from './components/HeaderComp.vue'
+import FooterComp from './components/FooterComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContentComp,
+    HeaderComp,
+    FooterComp
+  },
+  data () {
+    return {
+      componentType: 'MainComp'
+    }
+  },
+  methods: {
+    changeContent (componentType) {
+      this.componentType = componentType
+    }
   }
 }
+
 </script>
+
 
 <style>
 #app {
---
+++ src/components/MainComp.vue
@@ -0,0 +1,60 @@
+<template>
+  <component :is="componentType" />
+</template>
+
+<script>
+import ContentComp from './components/ContentComp.vue'
+
+export default {
+  name: 'MainComp',
+};
+</script>
+
+
+<style scoped>
+    html {
+    scroll-behavior: smooth;
+    }
+
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
+
+    a {
+    text-decoration: none;
+    }
+
+    a:visited {
+    color: inherit;
+    }
+
+    .center {
+    padding-left: calc(50% - 600px);
+    padding-right: calc(50% - 600px);
+    }
+
+    .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    }
+
+
+    .header {
+    width: 1200px;
+    padding: 50px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    }
+    .header__menubar {
+    display: flex;
+    gap: 50px;
+    color: rgb(41, 47, 54);
+    font-family: "Jost", sans-serif;
+    font-weight: 400;
+    font-size: 20px;
+    }
+</style>
